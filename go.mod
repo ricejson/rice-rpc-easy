d941a2f3dbad79357df9cb7d20964260ff6a15ab
ricejson <wjjbangbangbang@163.com>
2025-08-27 00:03:39 +0800

    add rpc web server code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ricejson/rice-rpc-easy
+
+go 1.23.8
---
+++ server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// WebServer 自定义web服务器对象
+type WebServer struct {
+}
+
+func NewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+// doStart 启动服务器
+func (s *WebServer) doStart(port int) {
+	// 设置请求处理器
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		// 处理请求
+		log.Println("Received request: " + request.Method + " " + request.RequestURI)
+		// 发送响应
+		writer.Header().Set("Content-Type", "text/plain")
+		writer.Write([]byte("Hello from rice HTTP server"))
+	})
+	// 启动服务器并监听端口
+	var portStr = strconv.Itoa(port)
+	if err := http.ListenAndServe(":"+portStr, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	} else {
+		log.Println("Server is now listening on port: " + portStr)
+	}
+}
